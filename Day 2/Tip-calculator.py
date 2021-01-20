ae7013696e6f5e1a78a7753543c8063fde6f5de4
Valentin <30319392+RusTNT@users.noreply.github.com>
2021-01-21 04:53:02 +0700

    Add files via upload

---
+++ Day 2/Tip-calculator.py
@@ -0,0 +1,6 @@
+print("Welcome to the tip calculator.")
+total = input("What was the total bill? $")
+tip = input("What percentage tip would you like to give? 10, 12, or 15? ")
+people = input("How many people to split the bill? ")
+summ = (((float(total) * (float(tip) / 100 ) + float(total) ) / float(people)))
+print(f"Each person should pay: ${str(round(summ, 2))}")
